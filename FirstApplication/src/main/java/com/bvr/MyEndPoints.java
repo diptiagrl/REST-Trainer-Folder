a471c4c8eb529757d6ee8261867906f540ecf701
vishymails <vishymails@gmail.com>
2023-02-20 12:12:56 +0530

    added

--- FirstApplication/src/main/java/com/bvr/MyEndPoints.java
+++ FirstApplication/src/main/java/com/bvr/MyEndPoints.java
@@ -1,5 +1,7 @@
 package com.bvr;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +17,16 @@ public class MyEndPoints {
 	@RequestMapping("/second")
 	public String sayHello(@RequestParam String name) {
 		return "Welcome : " + name;
+	}
+	
+	@RequestMapping(path="/secondpathvar/{name}")
+	public String sayHello2(@PathVariable String name) {
+		return "Path Definitions : " + name;
+	}
+	
+	
+	@GetMapping(path="/secondSampleBean")
+	public SecondSampleBean getSecondSampleBean() {
+		return new SecondSampleBean("For Citibank India");
 	}
 }
