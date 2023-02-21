e93f83f04364088464cf139f3d5e44a2f434a4fb
vishymails <vishymails@gmail.com>
2023-02-21 12:19:29 +0530

    added

--- admissions-service/src/main/java/com/bvr/AdmissionsService.java
+++ admissions-service/src/main/java/com/bvr/AdmissionsService.java
@@ -19,14 +19,14 @@ public class AdmissionsService {
 	
 	@RequestMapping("/physicians")
 	public EmployeeList getPhysicians() {
-		EmployeeList physicians = restTemplate.getForObject("http://localhost:8082/hr/employees", EmployeeList.class);
+		EmployeeList physicians = restTemplate.getForObject("http://hr-service/hr/employees", EmployeeList.class);
 		return physicians;
 	}
 	
 	
 	@RequestMapping("/commondiseases")
 	public DiseaseList getDiseases() {
-		DiseaseList diseases = restTemplate.getForObject("http://localhost:8083/pathology/diseases", DiseaseList.class);
+		DiseaseList diseases = restTemplate.getForObject("http://pathology-service/pathology/diseases", DiseaseList.class);
 		return diseases;
 	}
 }
--- admissions-service/src/main/java/com/bvr/AdmissionsServiceApplication.java
+++ admissions-service/src/main/java/com/bvr/AdmissionsServiceApplication.java
@@ -2,10 +2,13 @@ package com.bvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdmissionsServiceApplication {
 
 	public static void main(String[] args) {
@@ -13,6 +16,7 @@ public class AdmissionsServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
